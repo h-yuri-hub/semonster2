798fb4f34160aff1f9822027bc8fb71b18042959
yo-kanyukari <yokari@outlook.jp>
2021-07-19 10:31:01 +0900

    add: Monster classのモンスター召喚

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -1,6 +1,27 @@
 package semonster2;
 
+import java.util.List;
+
 public class Monster {
     private String name;
     private int rare;
+
+    public void toString() {
+        System.out.println(name+":レア度["+rare+"]");
+    }
+
+    public String summonMonster(int x) {
+        String[] monsterList={"ドラゴン","デュラハン","スライム","スケルトン","ドードリオ"};
+        
+        return monsterList[x];
+    
+
+    }
+        
+    Monster(int n,int r){
+        this.name=n;
+        this.rare=r;
+        this.toString();
+    }
+
 }
